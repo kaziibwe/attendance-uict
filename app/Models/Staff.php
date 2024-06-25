1b208333fa1aea516946c5316408dc5b9eeb87b0
alfredkaziibwe19@gmail.com <alfredkaziibwe19@gmail.com>
2024-06-25 09:08:32 -0400

    first hand on uict site

---
+++ app/Models/Staff.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Staff extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'title',
+        'email',
+        'staff_number',
+        'dob',
+        'current_appointment',
+        'appointment_date',
+        'nin',
+        'tin',
+        'phone',
+        'password',
+
+    ];
+    public $timestamps = false;
+}
