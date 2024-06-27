00247b6c8bbc6e8d4cef374250c48163693024f5
alfredkaziibwe19@gmail.com <alfredkaziibwe19@gmail.com>
2024-06-27 10:10:16 -0400

    update the apointments

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -46,5 +46,47 @@ class AppointmentController extends Controller
 
 
 
-  
+    public function updateAppointment(Request $request,$id)
+    {
+        try {
+
+
+            $Appointment = Appointment::find($id);
+            if(!$Appointment){
+               return response()->json(['message'=>'Appointment Not Found'],401);
+            }
+
+            $formFields = $request->validate([
+                'user_id' => 'required|exists:users,id',
+                'name' => 'required',
+                'descprition' => 'required',
+                'starting_date' => 'required',
+                'ending_date' => 'required',
+                'extended_date' => 'string',
+
+            ]);
+
+            $appointment = Appointment::where('id',$id)->update($formFields);
+
+            if ($appointment) {
+                return response()->json(["appointment" => $appointment, 'status' => true], 200);
+            } else {
+                return response()->json(['status' => false], 500);
+            }
+        } catch (ValidationException $e) {
+            // Return JSON response with validation errors
+            return response()->json([
+                'errors' => $e->errors(), // Detailed validation errors
+            ], 422);
+        } catch (\Exception $e) {
+            // Catch any other exceptions and return a generic error response
+            return response()->json([
+                'error' => $e->getMessage(), // Detailed error message
+            ], 500);
+        }
+    }
+
+
+
+
 }
